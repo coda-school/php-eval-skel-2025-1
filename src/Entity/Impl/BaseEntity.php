a701de5aa40f7ec479b843e1784e1c0281271a5c
EthanCarteron <ethan.carteron@gmail.com>
2026-01-22 17:13:36 +0100

    feat: version buggé pour Ethan sans la table tweet

--- src/Entity/Impl/BaseEntity.php
+++ src/Entity/Impl/BaseEntity.php
@@ -6,6 +6,7 @@ use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\User;
 
+#[ORM\MappedSuperclass]
 abstract class BaseEntity
 {
     #[ORM\Column(name: 'created_date', type: Types::DATETIME_MUTABLE, nullable: false, options: ['default' => "CURRENT_TIMESTAMP"])]
