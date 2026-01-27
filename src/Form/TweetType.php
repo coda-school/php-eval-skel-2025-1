132228ec7df58fa794347527f375baf342f08ddf
EthanCarteron <ethan.carteron@gmail.com>
2026-01-27 16:28:28 +0100

    fix: remise au bon endroit du HTML et dus CSS (à retravaillé, pas terminé et pas optimal)

---
+++ src/Form/TweetType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Tweet;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TweetType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('content', null, [
+                'label' => 'Quoi de neuf ?',
+                'attr' => ['placeholder' => 'Écrivez votre tweet ici...']
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Tweeter'
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tweet::class,
+        ]);
+    }
+}
